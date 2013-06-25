3127433671895b4b50d6ed050f6ab8c15c2b07c0
Konstantin Levinski <konstantin.levinski@gmail.com>
2013-06-25 08:05:21 +0800

    Fixed tool

--- server/src/tool/main.go
+++ server/src/tool/main.go
@@ -3,10 +3,18 @@ package main
 
 import (
 	"cloud"
+	"flag"
+	"log"
+	"os"
+	"path"
 )
 
+var storage_base = flag.String("store", path.Join(os.TempDir(), "cloud_storage"), "Location od the data")
+
 func main() {
-	print("API enabled @ port 8080\n")
-	cloud.Populate() // Test users
+	flag.Parse()
+	log.Print("API enabled @ port 8080")
+	log.Printf("Data is @ [%s]", *storage_base)
+	cloud.Configure(*storage_base) // Test users
 	cloud.Serve()
 }
