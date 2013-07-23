59d8c843d0b41b74be9051a88cd94628e7e9a917
Konstantin Levinski <kdl@vps.server.com>
2013-07-24 02:36:26 +0400

    Deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-killall tool
+killall tool-launcher.sh
+killall too
 STAMP=`date +deploy%Y%m%d%H%M%S`
 mkdir /tmp/$STAMP
 cd /tmp/$STAMP
 git init
-git pull ~/git/sheercloud
-go build tool
+git pull ~/git/sheercloud master
+export GOPATH=/tmp/$STAMP/server
+go build tool && cp ./tool ~/bin/tool 
+tool-launcher.sh &
