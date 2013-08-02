19acc8f58e0a271889328509e14ccaacb7df0009
Konstantin Levinski <konstantin.levinski@gmail.com>
2013-08-02 09:12:19 +0800

    Possible new fs interface. In design.

--- server/src/cloud/fs.go
+++ server/src/cloud/fs.go
@@ -14,7 +14,22 @@ package cloud
 */
 
 import (
-	_ "os"
-	_ "path"
-	_ "strings"
+	"io"
 )
+
+type UserName string
+type LocalPath string
+type OsPath string
+type FullPath string
+type File struct {
+	Path, ID string
+	Size     int
+}
+
+type FS interface {
+	Add(UserName, LocalPath, OsPath) error
+	Open(UserName, LocalPath) (io.Reader, error)
+	Del(UserName, LocalPath) error
+	List(UserName, LocalPath) ([]File, error)
+	Usage(UserName) int
+}
