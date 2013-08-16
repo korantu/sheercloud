dcdda46cdfc5ef5d5e8d597162d81a3f19f8ca83
Konstantin Levinski <konstantin.levinski@gmail.com>
2013-08-16 08:57:08 +0800

    API messages definition

---
+++ server/src/cloud/api.go
@@ -0,0 +1,63 @@
+package cloud
+
+/*
+
+  This file defines apis needed.
+
+  1. Login.
+  2. List users.
+  3. Update user (added if needed).
+
+  All goes through post.
+
+
+  Messages:
+   post
+
+*/
+
+// Common
+type ApiResource struct {
+	BytesAllowed, BytesUsed int
+}
+
+type ApiUser struct {
+	Name  string
+	Usage ApiResource
+}
+
+type ApiStatus struct {
+	Success bool
+}
+
+//--- #ApiDefinitions
+
+// /ui/login
+type ApiLoginRequest struct {
+	Username, Password string
+}
+
+type ApiLoginReply struct {
+	ApiStatus
+	Session string
+}
+
+// /ui/users
+type ApiUsersRequest struct {
+	Session string
+}
+
+type ApiUsersReply struct {
+	ApiStatus
+	Users []ApiUser
+}
+
+// /ui/adduser
+type ApiAddUserRequest struct {
+	Session        string
+	User, Password string
+}
+
+type ApiAddUsersReply struct {
+	ApiStatus
+}
