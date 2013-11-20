047ca840b09898fac85f43a53dc948c405261f88
Konstantin Levinski <konstantin.levinski@gmail.com>
2013-11-21 02:41:36 +0400

    IntelliJ Idea project

---
+++ server/src/main.go
@@ -0,0 +1,14 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Нонна
+ * Date: 21.11.13
+ * Time: 2:34
+ * To change this template use File | Settings | File Templates.
+ */
+package main
+
+import "fmt"
+
+func not_main() {
+	fmt.Printf("Hello world!")
+}
