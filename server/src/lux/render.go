3357e05be2fbf4b4a03612cb9f75e1bf5879df7b
Konstantin Levinski <konstantin.levinski@gmail.com>
2013-11-22 02:50:41 +0400

    Trivial rendering can be initiated and tested.

--- server/src/lux/render.go
+++ server/src/lux/render.go
@@ -4,9 +4,30 @@
 package lux
 
 import (
-	"cloud"
+	//"cloud"
+	"os/exec"
+	"log"
 )
 
+var LUX = "luxconsole"
+
+
+// Trivial render started
+func DoRender( scene, image  string) error{
+	path, err := exec.LookPath(LUX);
+	if err != nil {
+		return err
+	}
+
+	cmd := exec.Command(path, "-o", image, scene)
+	return cmd.Run()
+}
+
 func CheckLux() error {
-	return cloud.NewCloudError("Lux checking is not implemented")
+	path, err := exec.LookPath(LUX)
+	if err != nil {
+		return err
+	}
+	log.Printf("Found renderer at %s", path)
+	return nil
 }
